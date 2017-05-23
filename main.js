baab54249ef39e02371720c1e059d5f2e7b00b1e
Meka <mekaseymour@gmail.com>
2017-05-23 00:30:23 -0400

    resytled div 2, added first version of review form

--- main.js
+++ main.js
@@ -6,11 +6,54 @@ const locationInputField = document.getElementById('location-input-field');
 const submitLocationBtn = document.getElementById('submit-location-button');
 const errorMessage = document.getElementById('error-message');
 
+// DYNAMICALLY CREATE DOM ELEMENTS
+// dynamically build new review form
+const reviewFormDiv = document.createElement('div');
+const formTitle = document.createElement('h1');
+const reviewForm = document.createElement('form');
+const orgInput = document.createElement('input');
+const reviewTypesDiv = document.createElement('div');
+
+formTitle.innerHTML = 'What company/org would you like to review:'
+
+orgInput.setAttribute('type', 'text');
+orgInput.setAttribute('placeholder', 'company or organization');
+
+reviewForm.appendChild(orgInput);
+reviewFormDiv.appendChild(formTitle);
+reviewFormDiv.appendChild(reviewForm)
+reviewFormDiv.appendChild(reviewTypesDiv);
+
+let reviewTypesArr = ['gender/gender identity', 'race/ethnicity', 'education', 'religion', 'physical ability', 'sexual orientation'];
+
+let makeReviewTypesCheckboxes = (arr) => {
+  arr.forEach(type => {
+    let checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    let label = document.createElement('label');
+    label.innerHTML = type;
+    label.appendChild(checkbox);
+    reviewTypesDiv.appendChild(label);
+  });
+
+}
+
+makeReviewTypesCheckboxes(reviewTypesArr);
+
 // Get a reference to the firebase database
 const database = firebase.database();
 const databaseRootRef = database.ref('Orgs');
 // dataArr stores refreshed firebase data
 let dataArr = [];
+// googlePlaceSelected tracks whether or not the location entered into the
+// locationInputField was a proper Google autocomplete result
+let googlePlaceSelected = false;
+let searchResults = '';
+
+// div elements
+let makeReviewButton = document.createElement('button');
+makeReviewButton.innerHTML = 'new review';
+makeReviewButton.id = 'make-review-button';
 
 // Get Firebase data
 databaseRootRef.on('value', snapshot => {
@@ -23,11 +66,6 @@ databaseRootRef.on('value', snapshot => {
   console.log(dataArr);
 });
 
-// googlePlaceSelected tracks whether or not the location entered into the
-// locationInputField was a proper Google autocomplete result
-let googlePlaceSelected = false;
-let searchResults = '';
-
 // GOOGLE API - handle the location input field autocomplete
 let options = {
   // will only return cities
@@ -38,6 +76,8 @@ let options = {
 
 let autocomplete = new google.maps.places.Autocomplete(locationInputField, options);
 autocomplete.addListener('place_changed', () => {
+  // clear error message when a new place is selected
+  clearErrorMessage();
   let place = autocomplete.getPlace();
   if(place.address_components) {
     googlePlaceSelected = true;
@@ -105,13 +145,16 @@ $(document).ready(function() {
       results.forEach(resultObj => {
         showResults(resultObj);
       });
+      // append 'new review' button
+      secondDiv.appendChild(makeReviewButton);
+      secondDiv.appendChild(reviewFormDiv);
     } else {
       secondDiv.innerHTML = '';
       secondDiv.style.display = 'none';
       errorMessage.innerHTML = 'please enter a valid location'
     }
     // set locationAutoSelected back to false
-    locationAutoSelected = false;
+    googlePlaceSelected = false;
   }
 
   submitLocationBtn.addEventListener('click', scrollOneToTwo);
@@ -130,7 +173,7 @@ function firstDivPageClicks(event) {
   if(event.target !== locationInputField && event.target !== submitLocationBtn) {
     clearErrorMessage();
     if(locationInputField.value) {
-      if (locationAutoSelected) {locationInputField.size = locationInputField.value.length}
+      if (googlePlaceSelected) {locationInputField.size = locationInputField.value.length}
       // if location input isn't valid, go ahead and clear the field
       else {
         locationInputField.value = '';
@@ -149,6 +192,7 @@ function firstDivPageClicks(event) {
 // create dynamic list item templates
 function createListItem(title, subtitle) {
   let listItem = document.createElement('li');
+  listItem.className = 'reviews-list-item';
   let listItemTitle = document.createElement('h2');
   let listItemSubtitle = document.createElement('h4');
   //let listItemContent = document.createElement('p');
@@ -165,6 +209,7 @@ function createListItem(title, subtitle) {
 }
 
 function showResults(dataObj) {
+  // construct list item
   let reviewsList = document.createElement('ul');
   reviewsList.className = 'reviews-list';
   reviewsList.className = 'divTwoHeaders';
